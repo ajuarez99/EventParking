e55ca047454038dc279f6e2c7962ad1c373cf513
Allan Juarez <allanjuarez86@gmail.com>
2019-12-18 15:20:35 -0600

    refactored code so google maps is in service and mainscreen is in screens simplfying main.dart

--- eventparking/lib/main.dart
+++ eventparking/lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import './screens/MapScreen.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -11,57 +12,8 @@ class MyApp extends StatelessWidget {
       title: 'EventGo',
       theme: ThemeData(primarySwatch: Colors.red,),
       
-      home: MapSample(title: 'Event Parking'),
+      home: MapScreen(title: 'Event Parking'),
     );
   }
 }
 
-class MapSample extends StatefulWidget {
-MapSample({Key key, this.title}) : super(key: key);
-final String title;
-  @override
-  State<MapSample> createState() => MapSampleState();
-}
-
-class MapSampleState extends State<MapSample> {
-  Completer<GoogleMapController> _controller = Completer();
-
-  static final CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 14.4746,
-  );
-
-  static final CameraPosition _kLake = CameraPosition(
-      bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
-      tilt: 59.440717697143555,
-      zoom: 19.151926040649414);
-
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: GoogleMap(
-        mapType: MapType.satellite,
-        initialCameraPosition: _kGooglePlex,
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
-        },
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: _goToTheLake,
-        label: Text('To the lake!'),
-        icon: Icon(Icons.directions_boat),
-      ),
-    );
-  }
-
-  Future<void> _goToTheLake() async {
-    final GoogleMapController controller = await _controller.future;
-    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
-  }
-}
---
+++ eventparking/lib/screens/MapScreen.dart
@@ -0,0 +1,23 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import '../services/MapsService.dart';
+
+class MapScreen extends StatefulWidget {
+MapScreen({Key key, this.title}) : super(key: key);
+final String title;
+  @override
+  _MapScreen createState() => _MapScreen();
+}
+class _MapScreen extends State<MapScreen>{
+  MapsService googleMap = new MapsService();
+   @override
+  Widget build(BuildContext context) {
+   return googleMap.build(context);
+  }
+}
+
+ 
+
+ 
