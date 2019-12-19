cf1a88d46cafdcc7df4cdae892d7ff00e53a3bfb
Allan Juarez <allanjuarez86@gmail.com>
2019-12-19 00:34:28 -0600

    able to travel to a new page now

---
+++ eventparking/lib/screens/ProfileScreen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import '../widgets/BottomNavigation.dart';
+class ProfileScreen extends StatefulWidget {
+  ProfileScreen({Key key, this.name}) : super(key: key);
+  final String name;
+  @override
+  _ProfileScreen createState() => _ProfileScreen();
+}
+
+class _ProfileScreen extends State<ProfileScreen> {
+    @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: AppBar(
+        title: Text("Profile"),
+        centerTitle: true,
+      ),
+      bottomNavigationBar: BottomTravelBar(),
+    );
+  }
+}
--- eventparking/lib/widgets/BottomNavigation.dart
+++ eventparking/lib/widgets/BottomNavigation.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
 
+import 'package:eventparking/screens/MapScreen.dart';
+import 'package:eventparking/screens/ProfileScreen.dart';
 import 'package:flutter/material.dart';
 
 class BottomTravelBar extends StatefulWidget{
@@ -11,13 +13,33 @@ class _BottomTravelBar extends State<BottomTravelBar>{
 
   @override
   Widget build(BuildContext context){
+    var _currentIndex = 1;
   return  BottomNavigationBar(
-        
+       onTap:  (index) {
+          setState(() {
+                      _currentIndex = index;
+                    });
+         
+          if (index == 2){
+            Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => new ProfileScreen()),
+            );
+          }
+          else if(index == 1){
+             Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => new MapScreen(title: 'Event Parking')),
+            );
+          }
+        }, 
+      currentIndex: 0,
           type: BottomNavigationBarType.fixed ,
                   items: [
                     BottomNavigationBarItem(
                       icon: Icon(Icons.ac_unit,color: Color.fromARGB(255, 0, 0, 0)),
                       title: new Text('')
+                      
                     ),
                     BottomNavigationBarItem(
                       icon: Icon(Icons.home,color: Color.fromARGB(255, 0, 0, 0)),
@@ -25,7 +47,7 @@ class _BottomTravelBar extends State<BottomTravelBar>{
                     ),
                     BottomNavigationBarItem(
                       icon: Icon(Icons.person_outline,color: Color.fromARGB(255, 0, 0, 0)),
-                      title: new Text('Profile')
+                      title: new Text('Profile'),
                     ),        
                   ],
             );
