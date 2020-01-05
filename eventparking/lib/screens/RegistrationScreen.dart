ce3839225e84357fa8dd615fa8c51a797ead57a5
ajuarez99 <allanjuarez86@gmail.com>
2020-01-05 12:11:02 -0700

    sign up fields intiated

--- eventparking/lib/screens/RegistrationScreen.dart
+++ eventparking/lib/screens/RegistrationScreen.dart
@@ -16,6 +16,52 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
   RegistrationCustomText _usernameField;
 
   @override
+  void initState() {
+    super.initState();
+    _emailField = new RegistrationCustomText(
+      keyboard: TextInputType.emailAddress,
+      icon: Icons.email,
+      hint: "Email",
+    );
+
+    _passwordField = new RegistrationCustomText(
+      keyboard: TextInputType.text,
+      icon: Icons.lock,
+      hint: "Password",
+    );
+    _firstNameField = new RegistrationCustomText(
+      keyboard: TextInputType.text,
+      icon: Icons.person,
+      hint: "First Name",
+    );
+    _lastNameField = new RegistrationCustomText(
+      keyboard: TextInputType.text,
+      icon: Icons.person,
+      hint: "Last name",
+    );
+    _stateField = new RegistrationCustomText(
+      keyboard: TextInputType.text,
+      icon: Icons.satellite,
+      hint: "State",
+    );
+    _zipField = new RegistrationCustomText(
+      keyboard: TextInputType.number,
+      icon: Icons.check,
+      hint: "Zip",
+    );
+    _addressField = new RegistrationCustomText(
+      keyboard: TextInputType.text,
+      icon: Icons.home,
+      hint: "Address",
+    );
+    _usernameField = new RegistrationCustomText(
+      keyboard: TextInputType.text,
+      icon: Icons.person,
+      hint: "Username",
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
     return new Scaffold();
   }
